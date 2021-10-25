8fa9ebf352c0ec56c64b6997c4b3bc5faa021f37
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2021-10-25 10:55:27 -0400

    db update

---
+++ config/database.js
@@ -0,0 +1,5 @@
+// config/database.js
+module.exports = {
+	url: 'mongodb+srv://savageAuth:pIuybFhikT6dREN7@cluster0.qbdrb.mongodb.net/waterCounter?retryWrites=true&w=majority',
+	dbName: 'waterCounter',
+};
